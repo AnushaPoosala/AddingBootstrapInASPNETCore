6c9f1835535734d02275cbecfd7c426bf62658a8
AnushaPoosala <117409595+AnushaPoosala@users.noreply.github.com>
2024-03-30 12:18:44 -0400

    bootstrap, Login page

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using AddingBootstrapInASPNETCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddingBootstrapInASPNETCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products;
+        public ProductController() 
+        {
+            products = new List<Product>() { 
+                new Product() {ProductId=1,Name="LG",CategoryName="Digital",Description="Mobile",IsAvailable=true }, 
+                new Product() { ProductId=2,Name="LG",CategoryName="Digital",Description="Mobile",IsAvailable=true},
+                new Product() {ProductId=3,Name="LG",CategoryName="Digital",Description="Mobile",IsAvailable=true },
+                new Product() {ProductId=4,Name="LG",CategoryName="Digital",Description="Mobile",IsAvailable=true },
+                new Product() { ProductId=5,Name="LG",CategoryName="Digital",Description="Mobile",IsAvailable=true}
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+        public IActionResult Details(int prodNo)
+        {
+            var productsInfo = products.Where(p => p.ProductId == prodNo).FirstOrDefault();
+            return View(productsInfo);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
